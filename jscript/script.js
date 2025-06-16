71f0964f5db732725269e2e5c19e74ee1194eea1
PR135TT/CB-ADVERTISING <priestkaribo@gmail.com>
2025-06-16 22:56:51 +0100

    added easy loading

--- jscript/script.js
+++ jscript/script.js
@@ -71,6 +71,27 @@ document.querySelectorAll('#sidebar .nav-sidebar a')
   .forEach(link => link.addEventListener('click', () => {
     sidebar.classList.remove('active');
   }));
+  
+   document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll(".fade-in");
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("is-visible");
+        } else {
+          entry.target.classList.remove("is-visible");
+        }
+      });
+    }, {
+      threshold: 0.1 // Trigger when 10% is visible
+    });
+
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+  });
+
 
 
 
